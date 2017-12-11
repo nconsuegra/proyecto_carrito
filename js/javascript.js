932365e88854f0cf7a385d03c4ebf2b2446892bd
realboy85 <realboy85@hotmail.com>
2017-12-11 07:49:04 -0500

    Initial commit

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+//BASE DE DATOS
+ var productos = ["Antivirus", "Grafica", "Disco duro", "Ordenador", "Bolso portatil", "Portatil", "Memoria RAM", "Router Linux", "Sintonizadora TV"];
+ var precios = [33, 169, 36, 360, 11, 540, 21, 66, 25];
+ var precioTransporte = [6, 12, 20, "gratis"];
+ var IVA = 0.18;
+
+ function kart() {
+   document.getElementById('subtotal').innerHTML = "perrito";
+     }
